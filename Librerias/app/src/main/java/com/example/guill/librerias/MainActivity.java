c21c246c4ef861e4835246418c513746b6ed5743
guiller1712 <guillermo.exemur@gmail.com>
2017-02-10 11:14:07 -0700

    InputDialog
    Se creó el inputDialog para meter datos desde un diálogo.

--- Librerias/app/src/main/java/com/example/guill/librerias/MainActivity.java
+++ Librerias/app/src/main/java/com/example/guill/librerias/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.guill.librerias;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
@@ -9,12 +13,14 @@ import android.widget.Toast;
 
 import com.example.milibreria.PruebaLibreria;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView tv;
     Button btn;
     EditText et;
     Toast t;
+
+    int nSuma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +30,33 @@ public class MainActivity extends AppCompatActivity {
         et = (EditText)findViewById(R.id.editText);
         t = Toast.makeText(this,PruebaLibreria.texto,Toast.LENGTH_SHORT);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Cantidad de Números");
 
+        final EditText input = new EditText(this);
 
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        builder.setView(input);
+
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                nSuma = Integer.parseInt(input.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
