966c7b044f387f6eda28a38d1df84bab5848b558
Guillermo <guillermo.exemur@gmail.com>
2017-03-01 14:05:11 -0700

    Se agregó mensaje en LOGCAT

--- EvaluacionFinal_01/app/src/main/java/com/example/guill/evaluacionfinal_01/MainActivity.java
+++ EvaluacionFinal_01/app/src/main/java/com/example/guill/evaluacionfinal_01/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.guill.evaluacionfinal_01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.example.milibreria.Main;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Main m = new Main();
+        Log.d("Actividades",Main.pantallas);
     }
 }
